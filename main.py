85c302c6c6e016df0022eb484743b1073c99e1ab
Francesco Ioli <francesco.ioli@polimi.it>
2023-03-15 09:41:14 +0100

    added icepy libraries

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ from typing import List, Tuple, Union
 import exifread
 import numpy as np
 import PIL
+from PIL import Image
 from easydict import EasyDict as edict
+
+from lib.images import Image
 
 
 def parse_command_line() -> edict:
@@ -45,23 +48,40 @@ def parse_command_line() -> edict:
 def read_image_list(
     data_dir: Union[str, Path],
     image_ext: Union[str, List[str]] = None,
-    name_pattern: str = None,
+    # name_pattern: str = None,
     recursive: bool = False,
     case_sensitive: bool = False,
 ) -> List[Path]:
+    """
+    Returns a list of Path objects for all image files in a directory.
+
+    Args:
+        data_dir (Union[str, Path]): A string or Path object specifying the directory path containing image files.
+        image_ext (Union[str, List[str]], optional): A string or list of strings specifying the image file extensions to search for. Defaults to None, which searches for all file types.
+        name_pattern (str, optional): A string pattern to search for within image file names. Defaults to None.
+        recursive (bool, optional): Whether to search for image files recursively in subdirectories. Defaults to False.
+        case_sensitive (bool, optional): Whether to search for image files with case sensitivity. Defaults to False.
+
+    Returns:
+        [Path]: A list of Path objects for all image files found in the specified directory with the specified file extensions and name pattern.
+
+    Raises:
+        AssertionError: If the specified directory path is not valid.
+        AssertionError: If the specified image extension is not a string or list of strings with three characters each.
+
+    TODO:
+        Implement custom name patterns.
+
+    """
     data_dir = Path(data_dir)
     assert data_dir.is_dir(), "Error: invalid image directory."
 
     if image_ext is not None:
-        assert any(
-            [isinstance(image_ext, list), isinstance(image_ext, str)]
-        ), "Invalid input for image extension. It must be a 3 characters string (e.g., 'jpg') or a list of 3 characters strings (e.g., ['jpg', 'png'])"
+        msg = "Invalid input for image extension. It must be a 3 characters string (e.g., 'jpg') or a list of 3 characters strings (e.g., ['jpg', 'png'])"
+        assert any([isinstance(image_ext, list), isinstance(image_ext, str)]), msg
         if isinstance(image_ext, str):
             image_ext = [image_ext]
-        assert all(
-            [len(x) == 3 for x in image_ext]
-        ), "Wrong extension input. Please, provide extension with 3 characters only (e.g., jpg)"
-
+        assert all([len(x) == 3 for x in image_ext]), msg
         if not case_sensitive:
             # Make glob search case-insensitive
             image_ext_lower = [x.lower() for x in image_ext]
@@ -71,9 +91,6 @@ def read_image_list(
         ext_patt = [f".{x}" for x in image_ext]
     else:
         ext_patt = [""]
-
-        # ext_patt = ",".join(x for x in image_ext)
-        # ext_patt = f"{{{ext_patt}}}"
 
     files = []
     for ext in ext_patt:
@@ -88,9 +105,26 @@ def read_image_list(
     return files
 
 
+def rename_image(fname: Union[str, Path]) -> bool:
+    img = Image(fname)
+
+    return True
+
+
 if __name__ == "__main__":
     # args = parse_command_line()
     # data_dir = args.data_dir
 
     data_dir = Path("data")
     image_list = read_image_list(data_dir, image_ext=["jpg", "png"], recursive=True)
+
+    fname = image_list[0]
+    img = Image(fname)
+
+    # img = Image.open(fname)
+    # exif = img.getexif()
+
+    # with open(fname, "rb") as f:
+    #     exif = exifread.process_file(f, details=False)
+
+    print("Done.")
